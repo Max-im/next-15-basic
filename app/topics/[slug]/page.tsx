96ebf00faf4c7b308abdf6441f259946c456a099
max-im <pogidaevmo@gmail.com>
2025-02-26 17:07:32 +0200

    Add postList

--- app/topics/[slug]/page.tsx
+++ app/topics/[slug]/page.tsx
@@ -1,4 +1,6 @@
 import PostCreateForm from "@/components/PostCreateForm";
+import PostList from "@/components/PostList";
+import { fetchPostsBySlug } from "@/db/queries/post";
 
 interface TopicShowPageProps {
   params: Promise<{
@@ -11,10 +13,11 @@ export default async function TopicShowPage({ params }: TopicShowPageProps) {
 
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
-      <div className="col-span-4">
+      <div className="col-span-3">
         <h1 className="text-2xl font-bold mb-2">{slug}</h1>
+        <PostList fetchData={() => fetchPostsBySlug(slug)} />
       </div>
-      <div className="col-span-4">
+      <div className="col-span-1">
         <PostCreateForm slug={slug} />
       </div>
     </div>
---
+++ db/queries/post.ts
@@ -0,0 +1,32 @@
+import { Post } from "@prisma/client";
+import { db } from "..";
+
+export type PostWithData = (Post & {
+    topic: { slug: string };
+    user: { name: string | null };
+    _count: { comments: number };
+});
+
+
+export function fetchPostsBySlug(slug: string): Promise<PostWithData[]> {
+    return db.post.findMany({
+        where: { topic: { slug } },
+        include: {
+            topic: { select: { slug: true } },
+            user: { select: { name: true } },
+            _count: { select: { comments: true } }
+        }
+    })
+}
+
+export function fetchTopPosts(): Promise<PostWithData[]> {
+    return db.post.findMany({
+        take: 5,
+        orderBy: { createdAt: 'desc' },
+        include: {
+            topic: { select: { slug: true } },
+            user: { select: { name: true } },
+            _count: { select: { comments: true } }
+        }
+    });
+}
